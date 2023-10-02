b17c4180870c7f5acc0f1b0fa648d831f925ab3c
ADONIX\adoni <adonix56@gmail.com>
2023-10-01 23:41:37 -0700

    Initial Coffee Minigame Commit. Coffee moves left and right

---
+++ Assets/Scripts/CoffeeMiniGame/CoffeePot.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoffeeMinigame {
+    public class CoffeePot : MonoBehaviour {
+
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private float paddingDistance;
+
+        private Vector3 direction;
+        private SpriteRenderer spriteRenderer;
+
+        private void Start() {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            direction = Vector3.left;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            SetMoveDirection();
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+
+        private void SetMoveDirection() {
+            Vector2 minMax = new Vector2(spriteRenderer.bounds.min.x, spriteRenderer.bounds.max.x);
+            float screenMin = Camera.main.ScreenToWorldPoint(Vector3.zero).x + paddingDistance;
+            float screenMax = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0f)).x - paddingDistance;
+            if (minMax.x <= screenMin)
+                direction = Vector3.right;
+            if (minMax.y >= screenMax)
+                direction = Vector3.left;
+        }
+    }
+}
