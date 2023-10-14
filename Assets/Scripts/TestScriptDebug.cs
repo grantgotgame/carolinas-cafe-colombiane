e113ce2dc2f49314e7f7d29f99715f293546c454
Grant Brown <onlythebest@gmail.com>
2023-10-13 19:45:35 -0700

    you will charm the audiences: duplicate text fix

--- Assets/Scripts/TestScriptDebug.cs
+++ Assets/Scripts/TestScriptDebug.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public static class TestScriptDebug
 {
     //Change to true if needing to skip
-    public static bool isTesting = false;
+    public static bool isTesting = true;
 
     //Set which day you want
-    public static int chooseDay = 1;
+    public static int chooseDay = 4;
 
     //Set which character you want
     // 0 - Beatriz
@@ -16,5 +16,5 @@ public static class TestScriptDebug
     // 2 - Alejandro / Alex
     // 3 - Santiago
     // 4 - Acacia
-    public static int chooseCharacter = 0;
+    public static int chooseCharacter = 3;
 }
