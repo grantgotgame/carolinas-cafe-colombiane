ef68d162b60685ce7e6bbeeeca0a3e9c3e898430
ADONIX\adoni <adonix56@gmail.com>
2023-10-15 19:24:06 -0700

    Point System v2

--- Assets/Scripts/PointSystem.cs
+++ Assets/Scripts/PointSystem.cs
@@ -27,11 +27,20 @@ public static class PointSystem
     }
 
     public static int GetPoints(int id) {
-        /*
         switch (id) {
             case 0:
+                return PlayerPrefs.GetInt("Beatriz", 0);
+            case 1:
+                return PlayerPrefs.GetInt("Jairo", 0);
+            case 2:
+                return PlayerPrefs.GetInt("Alejandro", 0) + PlayerPrefs.GetInt("Alex", 0);
+            case 3:
+                return PlayerPrefs.GetInt("Santiago", 0);
+            default:
+                return PlayerPrefs.GetInt("Acacia", 0);
 
         }
+        /*
         if (character.ToString() == "Alejandro" || character.ToString() == "Alex") {
             return PlayerPrefs.GetInt("Alejandro", 0) + PlayerPrefs.GetInt("Alex", 0);
         }
